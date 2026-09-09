dcb00a70c1794b6ca3f9cc15b79ec5c8e3304c75
FHN <farhandwiyanakbar02@gmail.com>
2026-09-09 19:54:21 +0700

    feat: tambah create tasks dan menampilkan tasks
    menyelesaikan SRS-003

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', [TaskController::class, 'showAllTasks']);
+Route::get('/create', [TaskController::class, 'create']);
+Route::post('/create', [TaskController::class, 'store']);
