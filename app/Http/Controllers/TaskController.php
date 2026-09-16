294f081cc08c9a1497a693ff54d64211c7b3a448
FHN <farhandwiyanakbar02@gmail.com>
2026-09-16 19:42:30 +0700

    feat(tasks): tambah fitur edit, delete, dan menambahkan filter serta search pada showAllTasks
    mengimplementasikan SRS-003 dimana sebelumnya hanya ada fitur tambah dan showAllTasks di update sehingga terdapat filter berdasarkan status deadline sekaligus melengkapi fitur pada SRS-003

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,7 +9,7 @@ class TaskController extends Controller
 {
 
     public function create() {
-        return view('task.create');
+        return view('tasks.create');
     }
 
     public function store(Request $request) {
@@ -24,12 +24,56 @@ class TaskController extends Controller
 
         Task::create($validated);
 
-        return redirect()->route('task.index');
+        return redirect()->route('tasks.index');
     }
 
-    public function showAllTasks() {
-        $tasks = Task::query();
+    public function showAllTasks(Request $request) {
+        $filter = $request->input('filter');
+        $search = $request->input('search');
 
-        return view('task.index', compact('tasks'));
+        $tasks = Task::when($search, function ($query, $search) {
+            return $query->where('judul', 'like', '%' . $search . '%');
+        })
+        ->when($filter == 'low', function ($query, $value) {
+            return $query->where('prioritas', 'low');
+        })
+        ->when($filter == 'medium', function ($query, $value) {
+            return $query->where('prioritas', 'medium');
+        })
+        ->when($filter == 'high', function ($query, $value) {
+            return $query->where('prioritas', 'high');
+        })->paginate(50)->withQueryString();
+
+        return view('tasks.index', compact('tasks', 'filter', 'search'));
     }
+
+    public function editTaks(Request $request, Task $task) {    
+        $validated = $request->validate([
+            'judul' => 'required|string|max:255',
+            'deskripsi' => 'required',
+            'status' => 'required|string|max:50',
+            'prioritas' => 'required|string|max:50',
+            'deadline' => 'required|date',
+        ]);
+
+        $task->update($validated);
+
+        return redirect()->back()->with('success', 'task berhasil di edit');
+    }
+
+    public function showUpdateForm(Task $task) {
+        return view('tasks.update', compact('task'));
+    }
+
+    public function removeTask(Request $request) {
+        $task = Task::where('id', $request->id)->findOrFail();
+
+        $task->delete();
+
+        return redirect()->back()->with('success', 'task berhasil dihapus');
+    }
+
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/index', [TaskController::class, 'showAllTasks']);
-Route::get('/create', [TaskController::class, 'create']);
-Route::post('/create', [TaskController::class, 'store']);
+Route::get('/tasks/index', [TaskController::class, 'showAllTasks'])->name('showAllTasks');
+Route::get('/tasks/create', [TaskController::class, 'create'])->name('create-tasks');
+Route::post('/tasks/create', [TaskController::class, 'store'])->name('createTasks');
+Route::get('/tasks/update', [TaskController::class, 'showUpdateForm'])->name('task-update');
+Route::put('/tasks/update', [TaskController::class, 'editTask'])->name('updateTask');
+Route::delete('/tasks/delete', [TaskController::class, 'removeTask'])->name('deleteTask');
